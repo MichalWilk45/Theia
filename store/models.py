a759cbb63ab0d46c43bf1e84e9e457c49f20b10d
michalwilk45 <michalwilk45@gmail.com>
2023-10-14 17:36:58 +0200

    Products

--- store/models.py
+++ store/models.py
@@ -8,6 +8,11 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
+    class Meta:
+        verbose_name_plural = 'categories'
+
+
+
 class Store(models.Model):
     name=models.CharField(max_length=50)
     category=models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
