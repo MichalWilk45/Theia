499df9c64c6ceb2a09857eb96dc27e46f87707cb
michalwilk45 <michalwilk45@gmail.com>
2024-05-28 23:04:30 +0200

    postgres setup

--- theia_project/settings.py
+++ theia_project/settings.py
@@ -65,10 +65,21 @@ WSGI_APPLICATION = 'theia_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Theia',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',  # Set to 'localhost' if the DB is on the same machine
+        'PORT': '',  # Set to '' for default
     }
 }
 
