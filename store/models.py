b52911a59e82fe2710990b0f6d8d4d4f1d48d73e
michalwilk45 <michalwilk45@gmail.com>
2024-05-30 12:35:44 +0200

    cart

--- store/models.py
+++ store/models.py
@@ -65,3 +65,20 @@ class Order(models.Model):
 
 
 
+class Cart(models.Model):
+    user = models.OneToOneField(Customer, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Cart of {self.user.username}"
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name}"
+
+    def get_total_price(self):
+        return self.quantity * self.product.price
--- store/urls.py
+++ store/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('about', views.about, name='about'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('signup', views.signup, name='signup'),
+    path('admin/', admin.site.urls),
+    path('cart/', views.view_cart, name='view_cart'),
 ]
